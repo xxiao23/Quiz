7bb0c67b668859c2a856966dca94521aa901b2b0
Xiang Xiao <xxiao23@gmail.com>
2022-12-28 20:53:10 -0800

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Xiang Xiao on 12/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
